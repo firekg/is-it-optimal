b46d772835d86d2123a0d3f84bb478c3fca10637
Kevin Zhu <38451292+KevinOfCathay@users.noreply.github.com>
2019-04-24 16:05:53 -0400

    Add files via upload

--- Observation/main.py
+++ Observation/main.py
@@ -3,6 +3,6 @@ import Generate
 
 hypotable = Generate.Generate_Boundary_Hypo_Table(10)
 
-task = AL.ActiveLearning(6, 5, 2, knowledgeability=0.15)
+task = AL.ActiveLearning(knowledgeability=0.15)
 task.Set(user_hypo=hypotable)
 task.P_Task(0.1, 1.0, 0.1)
