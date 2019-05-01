0f62e8324e62043e5e16d01ee29798836a53616f
Kevin Zhu <38451292+KevinOfCathay@users.noreply.github.com>
2019-05-01 18:23:53 -0400

    Delete Main.py

--- Observation/Main.py
+++
@@ -1,7 +0,0 @@
-import AL
-
-task = AL.ActiveLearning(knowledgeability=1.0)
-task.Set(user_hypo=[[1, 1, 1], [1, 1, 0], [1, 0, 0], [0, 0, 0]])
-task.K_Task(1000)
-print(task.p_learner_h_xy)
-task.P_Task()
