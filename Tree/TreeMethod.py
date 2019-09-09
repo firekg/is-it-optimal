7ea5d79071c7cf1ea2ebf5c56c16d0aec6623400
Kevin Zhu <38451292+KevinOfCathay@users.noreply.github.com>
2019-09-09 11:37:14 -0400

    Add files via upload

--- Tree/TreeMethod.py
+++ Tree/TreeMethod.py
@@ -8,6 +8,7 @@ import Generate
 # Generate the hypothesis matrix
 total_features = 4
 hypo = Generate.Boundary_Hypo_Table(total_features, True)
+total_hypos = len(hypo)
 
 # Create the observation list
 arr = numpy.empty(total_features, dtype=int)
@@ -17,18 +18,31 @@ for x in range(total_features):
 obs_list = []
 Utility.Permutation(arr, total_features, total_features, obs_list)
 
-for true_idx in range(total_features):
-    true_hypo = hypo[1]
+best_route = {}
+
+for true_idx in range(total_hypos):
+    # Create the true hypo
+    true_hypo = hypo[true_idx]
     print("True hypothesis = ", true_hypo)
 
+    maximum = 0
+    route = obs_list[0]
     for obs in obs_list:
         print("List = ", obs)
         # The observed feature list
         obsd_list = []
         idx = 0
+        sum = 0
         # Start to observe
         while idx < total_features:
             obsd_list.append(obs[idx])
             P = Utility.Observe(hypo, true_hypo, obsd_list)
+            sum += P
             print("Observed = ", obsd_list, "  P = ", P)
             idx += 1
+        if sum > maximum:
+            route = obs
+            maximum = sum
+    best_route[tuple(true_hypo)] = route
+
+print(best_route)
